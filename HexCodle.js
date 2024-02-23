56c171509592c89d0712b9e20ca233c8da3dce66
vcpak614 <vcpak614@gmail.com>
2024-02-22 22:35:17 -0800

    Var. changes

--- HexCodle.js
+++ HexCodle.js
@@ -1,7 +1,7 @@
-const menuIcon = document.querySelector('.menu-icon');
-const offScreenMenu = document.querySelector('.off-screen-menu');
+const osmMainIcon = document.querySelector('.osm-main-icon');
+const osmMain = document.querySelector('.osm-main');
 
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+osmMainIcon.addEventListener('click', () => {
+    osmMainIcon.classList.toggle('active');
+    osmMain.classList.toggle('active');
 })
