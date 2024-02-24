224c82fe90b459009d6f610c2bc4b40eecf3ee29
vcpak614 <vcpak614@gmail.com>
2024-02-24 09:34:01 -0800

    General changes

--- HexCodle.js
+++ HexCodle.js
@@ -1,16 +1,15 @@
-const osmMain = document.querySelector('.osm-main');
-const osmMainIcon = document.querySelector('.osm-main-icon');
+const osmMain = document.getElementsByClassName('.osm-main');
+const osmMainIcon = document.getElementsByClassName('.osm-main-icon');
 
-const osmRules = document.querySelector('.osm-rules');
-const osmRulesIcon = document.querySelector('.osm-rules-icon');
+const osmRules = document.getElementsByClassName('.osm-rules');
+const osmRulesIcon = document.getElementsByClassName('.osm-rules-icon');
 
-const osmStats = document.querySelector('.osm-stats');
-const osmStatsIcon = document.querySelector('.osm-stats-icon');
+const osmStats = document.getElementsByClassName('.osm-stats');
+const osmStatsIcon = document.getElementsByClassName('.osm-stats-icon');
 
-const osmSettings = document.querySelector('.osm-settings');
-const osmSettingsIcon = document.querySelector('.osm-settings-icon');
+const osmSettings = document.getElementsByClassName('.osm-settings');
+const osmSettingsIcon = document.getElementsByClassName('.osm-settings-icon');
 
 osmMainIcon.addEventListener('click', () => {
     osmMain.classList.toggle('active');
-    console.log("active!");
 })
