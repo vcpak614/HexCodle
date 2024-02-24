f3163b6035abc14a044affa8b52060e4482e1b65
vcpak614 <vcpak614@gmail.com>
2024-02-24 09:14:56 -0800

    New initial

--- HexCodle.js
+++ HexCodle.js
@@ -1,7 +1,16 @@
-const osmMainIcon = document.querySelector('.osm-main-icon');
 const osmMain = document.querySelector('.osm-main');
+const osmMainIcon = document.querySelector('.osm-main-icon');
+
+const osmRules = document.querySelector('.osm-rules');
+const osmRulesIcon = document.querySelector('.osm-rules-icon');
+
+const osmStats = document.querySelector('.osm-stats');
+const osmStatsIcon = document.querySelector('.osm-stats-icon');
+
+const osmSettings = document.querySelector('.osm-settings');
+const osmSettingsIcon = document.querySelector('.osm-settings-icon');
 
 osmMainIcon.addEventListener('click', () => {
-    osmMainIcon.classList.toggle('active');
     osmMain.classList.toggle('active');
+    console.log("active!");
 })
